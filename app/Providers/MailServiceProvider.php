4efe449688f38894937cbf0ce5001f2c11fad7e4
unknown <duylong0601@gmail.com>
2025-02-10 22:16:38 +0700

    update_1002

--- app/Providers/MailServiceProvider.php
+++ app/Providers/MailServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class MailServiceProvider extends ServiceProvider
@@ -13,11 +14,13 @@ class MailServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $config = DB::table('mail_configs')->first();
-
-        if ($config) {
-            Config::set('mail.mailers.smtp.username', $config->username ?? '');
-            Config::set('mail.mailers.smtp.password', $config->password ?? '');
+        if (Schema::hasTable('mail_configs')) {
+            $config = DB::table('mail_configs')->first();
+    
+            if ($config) {
+                Config::set('mail.mailers.smtp.username', $config->username ?? '');
+                Config::set('mail.mailers.smtp.password', $config->password ?? '');
+            }
         }
     }
 
